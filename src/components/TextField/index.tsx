2c147b2be33761d3148ba67d4dcec22043904c41
MinwookJo <alsdnr0537@naver.com>
2019-07-16 00:03:59 +0900

    MJ: Add json-server
    - Install json-server
    - Add script to start json-server
    - Add db.json file

--- src/components/TextField/index.tsx
+++ src/components/TextField/index.tsx
@@ -3,9 +3,8 @@ import React from "react";
 class TextField extends React.Component {
     render() {
         return(
-            <div>
-                
-            </div>
+            <input>
+            </input>
         );
     }
 }
